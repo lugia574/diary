3db9ebea1340ba122bc8b97ab9a282d1b609441c
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-02-23 15:14:06 +0900

    00

--- study/java/code/algo/l2_피보나치.java
+++ study/java/code/algo/l2_피보나치.java
@@ -4,18 +4,18 @@ package study.Java.code.algo;
 // 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
 // 제한 사항 n은 2 이상 100,000 이하인 자연수입니다.
 
-class Solution {
+class Solution1 {
     public int solution(int n) {
         int[] res = new int[n+1];
         
         for (int i = 0; i < res.length; i++) {
             if (i <= 1) res[i] = i;
             else{
-                res[i] = res[i-1] + res[i-2];
+                res[i] = (res[i-1] + res[i-2]) % 1234567;
             }
         }
         
-        int answer = res[n] % 1234567;
+        int answer =  res[n] % 1234567;
 
         return answer;
     }
@@ -25,9 +25,8 @@ public class l2_피보나치 {
     public static void main(String[] args) {
         int n = 3;
         int res = 2;
-        Solution sol = new Solution();
-        System.out.println("안녕");
-        int ans = sol.solution(n);
+        Solution1 sol = new Solution1();
+        int ans = sol.solution(3);
         System.out.println(ans);
         System.out.println(res == ans ? "True":"False");
     }
