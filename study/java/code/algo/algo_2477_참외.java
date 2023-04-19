66ea1071402605f56cf8cb2aa356bccc5cd0a6a6
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-19 19:00:15 +0900

    ㅇㅇ

---
+++ study/java/code/algo/algo_2477_참외.java
@@ -0,0 +1,14 @@
+package study.Java.code.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algo_2477_참외 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+    }
+}
