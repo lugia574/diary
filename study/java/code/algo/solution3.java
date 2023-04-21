a574fcd075bbf51c39e6739a35be4ae91f9213f2
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-21 21:02:44 +0900

    ㅇㅇ

--- study/java/code/algo/solution3.java
+++ study/java/code/algo/solution3.java
@@ -5,16 +5,14 @@ class solution3 {
         boolean answer = true;
         Stack<Character> stack = new Stack<Character>();
         
-        if(s.charAt(0) == ')')
-            return false;
-        
-        
         for(int i=0; i<s.length(); i++){
-            if(!stack.isEmpty() && s.charAt(i) == ')'){
-                stack.pop();
-            }
+            if(s.charAt(i) == '(')  // 현재 (가 들어갈 자리면 스택에 넣는다.
+            stack.push('(');
             else{
-                stack.push(s.charAt(i));
+            if(stack.isEmpty()) // 현재 )가 들어갈 자리인데 스택이 비어있을경우 -> false
+                return false;
+            else
+                stack.pop();    // 현재 )가 들어갈 상태에서 스택에 괄호('(')가 있는경우 -> pop
             }
         }
 
