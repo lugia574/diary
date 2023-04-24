401f9c2cecee7dc7df216744fce6a07923f69bee
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-24 18:49:17 +0900

    ㅇㅇ

---
+++ study/java/code/algo2/hash/algo_베스트앨범.java
@@ -0,0 +1,74 @@
+// 이게 개 같은게 Tim Sort 를 알아야함
+// https://d2.naver.com/helloworld/0315536
+// https://gogigood.tistory.com/70
+
+package study.Java.code.algo2.hash;
+import java.util.*;
+public class algo_베스트앨범 {
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        int[] ans = solution(genres, plays);
+
+        System.out.println(ans.toString());
+    }
+    static class Music{
+        String genre;
+        int play;
+        int idx;
+
+        public Music(String genre, int play, int idx){
+            this.genre = genre;
+            this.play = play;
+            this.idx = idx;
+        }
+    }
+
+    private static int[] solution(String[] genres, int[] plays) {
+        int len = genres.length;
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < len; i++)
+            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
+        
+        ArrayList<String> ordered = new ArrayList<>();
+
+        while(map.size() > 0){
+            int max = -1;
+            String max_key = "";
+            for(String k : map.keySet()){
+                int tmp = map.get(k);
+                if(tmp > max){
+                    max = tmp;
+                    max_key = k;
+                }
+            }
+
+            ordered.add(max_key);
+            map.remove(max_key);
+        }
+
+        ArrayList<Music> result = new ArrayList<>();
+        for(String g : ordered){
+            ArrayList<Music> list = new ArrayList<>();
+            for(int i = 0; i < len; i++){
+                if(genres[i].equals(g)){
+                    list.add(new Music(genres[i], plays[i], i));
+                }
+            }
+            Collections.sort(list, (o1, o2) -> o2.play - o1.play); // 내림차순 소팅
+            result.add(list.get(0)); 	// 1개는 무조건 수록
+            if(list.size()!=1){ 	// 더 수록할 곡이 있으면(==장르 내의 노래가 1개보다 많으면) 수록
+                result.add(list.get(1));
+            }
+        }
+
+        // print result
+        int[] answer = new int[result.size()];
+        for(int i=0; i<result.size(); i++){
+            answer[i] = result.get(i).idx;
+        }
+        return answer;
+    
+        }
+    }
---
+++ study/java/code/algo2/stack/algo_같은숫자싫.java
@@ -0,0 +1,26 @@
+package study.Java.code.algo2.stack;
+import java.util.*;
+public class algo_같은숫자싫 {
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        int[] res = {1,3,0,1};
+        //int[] ans = solution(arr);
+        // 뭐냐 맞는데 왜 틀렸다고 하냐
+        //System.out.println(res.equals(ans)? "정답입니다" : "틀렸습니다 모지리 새끼야");
+    }   
+
+    private static int[] solution(int []arr) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        int len = arr.length;
+        int value = -1;
+        for(int i=0; i<len; i++) {
+            if(arr[i] != value) {
+                answer.add(arr[i]);
+                value = arr[i];
+            }
+        }
+        int[] ans = new int[answer.size()];
+        for(int j = 0; j < answer.size(); j++) {ans[j] = answer.get(j);}
+        return ans;
+    }
+}
