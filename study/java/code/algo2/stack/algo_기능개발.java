da85cc5c4b11ca137f1fc785e22a163dcae3ec73
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-24 22:14:24 +0900

    ㅇㅇ

---
+++ study/java/code/algo2/stack/algo_기능개발.java
@@ -0,0 +1,35 @@
+package study.Java.code.algo2.stack;
+import java.util.*;
+public class algo_기능개발 {
+    public static void main(String[] args) {
+        int[] progresses = {1, 1, 1, 1};
+        int[] speeds = {100, 50, 99, 100};
+        int[] res = {1, 2, 1};
+
+        int[] ans = solution(progresses, speeds);
+        for (int i = 0; i < ans.length; i++) {
+            //System.out.println(ans[i]);
+        }
+    }
+
+    private static int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            queue.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
+        }
+        
+        List<Integer> answer = new ArrayList<>();
+        while (!queue.isEmpty()){
+            int day = queue.poll();
+            int cnt = 1;
+            
+            while(!queue.isEmpty() && day >= queue.peek()){
+                cnt++;
+                queue.poll();
+            }
+            answer.add(cnt);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ study/java/code/algo2/stack/algo_다리트럭.java
@@ -0,0 +1,49 @@
+// https://minhamina.tistory.com/241
+
+package study.Java.code.algo2.stack;
+
+import java.util.*;
+
+public class algo_다리트럭 {
+    public static void main(String[] args) {
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
+        int res = 8;
+
+        int ans = solution(bridge_length, weight, truck_weights);
+        System.out.println(ans+ " 아닌데 뭐냐 시이발?");
+    }
+
+    private static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+        int len = truck_weights.length;
+        int tsum = 0;
+        for(int i = 0; i < len; i++){
+            int truck = truck_weights[i];
+
+            while(true){
+                if (q.isEmpty()){
+                    q.offer(truck);
+                    tsum += truck;
+                    answer ++;
+                    break;
+                }else if (q.size() == bridge_length) tsum -= q.poll();
+                else{
+                    if(tsum + truck <= weight){
+                        q.offer(truck);
+                        tsum += truck;
+                        answer ++;
+                        break;
+                    }else{
+                        q.offer(0);
+                        answer ++;
+                    }
+                }
+            }
+        }
+
+        return answer + bridge_length;
+    }
+}
