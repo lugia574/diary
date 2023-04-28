9fbb3a9ed265d6a49cf00d577a9c200b1fa8f869
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-04-28 21:57:06 +0900

    ㅇㅇ

---
+++ study/면접 지식/SQL/문제/value/최댓r값최솟값.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+
+-- 가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+SELECT MAX(DATETIME) AS 시간
+FROM ANIMAL_INS;
+
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59038
+
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+SELECT MIN(DATETIME) AS 시간 FROM ANIMAL_INS;
