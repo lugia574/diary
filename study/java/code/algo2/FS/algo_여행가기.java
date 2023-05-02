28b2e9d5a250855d9d98b7e59ab15eb27552c748
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-02 16:53:51 +0900

    ㅇㅇ

--- study/java/code/algo2/FS/algo_여행가기.java
+++ study/java/code/algo2/FS/algo_여행가기.java
@@ -1,76 +1,40 @@
+// https://velog.io/@rari_1110/DFS-프로그래머스-여행경로-JAVA
 package study.Java.code.algo2.FS;
 import java.util.*;
 
 public class algo_여행가기 {
-    static List<Stack<String>> result;
-    static String[][] arr;
+    static boolean[] visited;
+    static ArrayList<String> allRoute; 
 
     public static String[] solution(String[][] tickets) {
-        result = new ArrayList<>();
-        arr = tickets;  
+            String[] answer = {};
+            int cnt = 0;
+            visited = new boolean[tickets.length];
+            allRoute = new ArrayList<>();
+            
+            dfs("ICN", "ICN", tickets, cnt);
+            
+            Collections.sort(allRoute);
+            answer = allRoute.get(0).split(" ");
+            
+            return answer;
 
-        boolean[] visited = new boolean[arr.length];
-        Stack<String> st = new Stack<>();
-        st.push("ICN");
-
-        dfs(visited, st, 0);
-
-        if (result.size() > 1) {
-            Collections.sort(result, new Comparator<Stack<String>>() {
-                @Override
-                public int compare(Stack<String> o1, Stack<String> o2) {
-                    for (int i = 0; i < o1.size(); i++) {
-                        String s1 = o1.get(i);
-                        String s2 = o2.get(i);
-
-                        if (!s1.equals(s2)) {
-                            return s1.compareTo(s2);
-                        }
-                    }
-
-                    return 0;
-                }
-            });
-        }
-
-        Stack<String> res = result.remove(0);
-        String[] answer = new String[res.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = res.get(i);
-        }
-
-        return answer;
     }
 
-    public static void dfs(boolean[] visited, Stack<String> st, int len) {
-        if (len == arr.length) {
-            Stack<String> res = new Stack<>();
-            for (String s : st) {
-                res.push(s);
-            }
-
-            result.add(res);
+    public static void dfs(String start, String route, String[][] tickets, int cnt) {
+        if(cnt == tickets.length){
+            allRoute.add(route);
             return;
         }
-
-        String arrive = st.peek();
-
-        for (int i = 0; i < arr.length; i++) {
-            String[] tic = arr[i];
-
-            if (!visited[i] && arrive.equals(tic[0])) {
-                st.push(tic[1]);
+        
+        for(int i=0; i<tickets.length; i++){
+            if(start.equals(tickets[i][0]) && !visited[i]){
                 visited[i] = true;
-
-                dfs(visited, st, len + 1);
-
+                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
                 visited[i] = false;
-                st.pop();
             }
         }
     }
-
 
     public static void main(String[] args) {
         String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
