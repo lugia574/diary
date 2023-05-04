333b5779993ee0feabcd2479ddb7b85bf0c95d0f
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-05-04 15:43:39 +0900

    ㅇㅇ

---
+++ study/java/code/algo2/FS/DFS/algo_1707_이분그래프.java
@@ -0,0 +1,38 @@
+// package study.Java.code.algo2.FS.DFS;
+
+// // https://jellyinghead.tistory.com/14
+// // 병신같은 이클립스가 그냥 멍청하게 오류를 처 뱉고 있음
+
+// import java.util.*;
+// import java.io.*;
+
+// public class algo_1707_이분그래프 {
+// 	static int v, e;
+// 	static ArrayList<Integer>[] graph;
+// 	static int visit[];
+//     public static void main(String[] args) throws Exception{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+//         int T = Integer.parseInt(st.nextToken());
+
+//         for (int tr = 0; tr < T; tr++) {
+//             st = new StringTokenizer(br.readLine());
+//             v = Integer.parseInt(st.nextToken());
+//             e = Integer.parseInt(st.nextToken());
+//             visit = new int[v + 1];
+//             graph = new ArrayList[v+1];
+//             for (int i = 0; i <= v; i++) {
+//                 graph[i] = new ArrayList<Integer>();
+//             }
+    
+//             for (int i = 0; i < e; i++) {
+//                 st = new StringTokenizer(br.readLine());
+//                 int x = Integer.parseInt(st.nextToken());
+//                 int y = Integer.parseInt(st.nextToken());
+    
+//                 // graph[x].add(y);
+//                 // graph[y].add(x);
+//             }
+//         }
+//     }
+// }
---
+++ study/java/code/algo2/FS/algo_아이템줍기.java
@@ -0,0 +1,25 @@
+package study.Java.code.algo2.FS;
+
+
+public class algo_아이템줍기 {
+    static int[][] board;
+    static int answer;
+
+    private static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] rectangle = {{1,1,7,4}, {3,2,5,5}, {4,3,6,9}, {2,6,8,8}};
+        int characterX = 1;
+        int characterY = 3;
+        int itemX = 7;
+        int itemY = 8;
+        int res = 17;
+
+
+        int ans = solution(rectangle, characterX, characterY, itemX, itemY);
+        
+    }
+}
