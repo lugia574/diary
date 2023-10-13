f0843d7fd876fc6eebe1b6c302605bd6f067aa98
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-10-13 20:10:53 +0900

    ㅇㅇ

--- study/JavaScript/algo/algo_전화번호목록.js
+++ study/JavaScript/algo/algo_전화번호목록.js
@@ -17,3 +17,69 @@ function solution(clothes) {
 
   return answer;
 }
+
+// 베스트 앨범ㄴ
+function solution(genres, plays) {
+  const len = genres.length;
+  const map = new Map();
+
+  for (let i = 0; i < len; i++) {
+    const genre = genres[i];
+    map.set(genre, (map.get(genre) || 0) + plays[i]);
+  }
+
+  const ordered = [];
+
+  while (map.size > 0) {
+    let max = -1;
+    let maxKey = "";
+
+    for (const key of map.keys()) {
+      const tmp = map.get(key);
+      if (tmp > max) {
+        max = tmp;
+        maxKey = key;
+      }
+    }
+
+    ordered.push(maxKey);
+    map.delete(maxKey);
+  }
+
+  const result = [];
+
+  for (const g of ordered) {
+    const list = [];
+
+    for (let i = 0; i < len; i++) {
+      if (genres[i] === g) {
+        list.push(new Music(genres[i], plays[i], i));
+      }
+    }
+
+    list.sort((a, b) => b.play - a.play); // 내림차순 소팅
+    result.push(list[0]); // 1개는 무조건 수록
+
+    if (list.length > 1) {
+      // 더 수록할 곡이 있으면 (장르 내의 노래가 1개보다 많으면) 수록
+      result.push(list[1]);
+    }
+  }
+
+  // print result
+  const answer = [];
+
+  for (let i = 0; i < result.length; i++) {
+    answer[i] = result[i].idx;
+  }
+
+  return answer;
+}
+
+class Music {
+  constructor(genre, play, idx) {
+    this.genre = genre;
+    this.play = play;
+    this.idx = idx;
+  }
+}
