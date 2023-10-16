116a0341d2ea86c8942bea80903aa14139ab4098
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-10-16 18:01:55 +0900

    ㅇㅇ

--- study/JavaScript/algo/algo_기초100문제.js
+++ study/JavaScript/algo/algo_기초100문제.js
@@ -7,3 +7,29 @@ function solution(numer1, denom1, numer2, denom2) {
   const gcdNum = gcd(denom, numer);
   return [numer / gcdNum, denom / gcdNum];
 }
+
+//////
+// 이건 reduce 함수 배열 순회하면서 a를 뽑아서 b (기존값)에 적용하는거 맨 뒤 매개변수는 기존값 세팅
+function solution(numbers) {
+  const sumNum = numbers.reduce((a, b) => a + b, 0);
+  return sumNum / numbers.length;
+}
+
+/// repeat 애는 또 뭐냐 보니까 뭔소린지는 알겠는데 흠흠흠
+function solution(my_string, n) {
+  var answer = [...my_string].map((v) => v.repeat(n)).join("");
+  console.log(answer);
+  return answer;
+}
+
+// 맵을 이렇게 해버리네
+function solution(strlist) {
+  return strlist.map((el) => el.length);
+}
+
+// 이건 생각 못했네
+// 대충 abcdea 라는 문자열 있을때 letter 값이 a 면 bcde 반환 해주는건데 이걸 이렇게 하네
+function solution(my_string, letter) {
+  const answer = my_string.split(letter).join("");
+  return answer;
+}
