a33ed2a3cb629f2b1f85251969b11c7ff39fdbb7
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-10-29 12:27:22 +0900

    ㅇㅇ

---
+++ study/JavaScript/algo/algo_l1문제 모음.js
@@ -0,0 +1,23 @@
+// 약수들의 합
+const cd = (a) => {
+  const cdSet = new Set();
+  const sqrtNum = Math.floor(Math.sqrt(a)) + 1;
+  for (let i = 1; i < sqrtNum; i++) {
+    if (a % i === 0) {
+      cdSet.add(i);
+      cdSet.add(a / i);
+    }
+  }
+  return Array.from(cdSet);
+};
+function solution(n) {
+  const arr = cd(n);
+  return arr.reduce((a, cur) => a + cur, 0);
+}
+
+// 자리수 더하기
+function solution(n) {
+  return [...n.toString()].reduce((a, cur) => parseInt(a) + parseInt(cur), 0);
+}
+
+// 문자열을 정수로 바꾸기
