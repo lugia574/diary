b8f211c484ef4859401609d90a33efc75e3158e2
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-10-31 16:25:15 +0900

    ㅇㅇ

--- study/JavaScript/algo/algo_1문제 모음.js
+++ study/JavaScript/algo/algo_1문제 모음.js
@@ -36,3 +36,76 @@ function solution(s) {
   const arr = [...s].sort((a, b) => b.charCodeAt() - a.charCodeAt());
   return arr.join("");
 }
+
+// 최대공약수, 최소공배수 섹섹
+function solution(n, m) {
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+  const gcdNum = gcd(n, m);
+  return [gcdNum, (m * n) / gcdNum];
+}
+
+// toUpperCase, toLowerCase 둘다 쓴 문제
+// 이상한 문자 만들기
+function solution(s) {
+  const arr = s.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    const word = [...arr[i]];
+    for (let j = 0; j < word.length; j++) {
+      if (j % 2 === 0) word[j] = word[j].toUpperCase();
+      else word[j] = word[j].toLowerCase();
+    }
+    arr[i] = word.join("");
+  }
+  return arr.join(" ");
+}
+
+// 삼총사
+// 자체 combination 함수 만들었음
+function solution(number) {
+  let answer = 0;
+  const combin = (arr, idx, com, l) => {
+    if (l === 3) {
+      if (com.reduce((a, cur) => a + cur, 0) === 0) answer++;
+    }
+
+    for (let i = idx; i < arr.length; i++) {
+      com.push(arr[i]);
+      combin(arr, i + 1, com, l + 1);
+      com.pop();
+    }
+  };
+  combin(number, 0, [], 0);
+  return answer;
+}
+
+// 시저암호
+// a 97 z 122
+// A 65 Z 90
+function solution(s, n) {
+  let answer = "";
+  const arr = [...s];
+  arr.forEach((e) => {
+    const charNum = e.charCodeAt();
+    if (64 < charNum && charNum < 91)
+      answer += String.fromCharCode(
+        charNum + n > 90 ? charNum + n - 91 + 65 : charNum + n
+      );
+    else if (96 < charNum && charNum < 123)
+      answer += String.fromCharCode(
+        charNum + n > 122 ? charNum + n - 123 + 97 : charNum + n
+      );
+    else answer += e;
+  });
+  return answer;
+}
+
+// 문자열 마음대로 정렬하기
+// 아 문자열 정렬 어렵네~
+// 자세한건 https://gurtn.tistory.com/184 여기 보고 합시다~
+function solution(strings, n) {
+  strings.sort(
+    (a, b) =>
+      [...a][n].charCodeAt() - [...b][n].charCodeAt() || (a < b ? -1 : 1)
+  );
+  return strings;
+}
