64ad42f60edc929e7b8b6e85514caa7cd1720f01
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-11-19 14:28:23 +0900

    ㅇㅇ

--- study/JavaScript/algo/algo_1문제 모음.js
+++ study/JavaScript/algo/algo_1문제 모음.js
@@ -384,3 +384,59 @@ function solution(new_id) {
 }
 
 // 개인정보 수집 유효기간
+function solution(today, terms, privacies) {
+  let current = today.split(".").map((v) => +v);
+  let termsKind = {};
+  terms.forEach((v) => {
+    let [kind, period] = v.split(" ");
+    termsKind[kind] = period;
+  });
+
+  let result = [];
+
+  privacies.forEach((v, i) => {
+    let [past, kind] = v.split(" ");
+    let [y, m, d] = past.split(".").map((v) => +v);
+    m += +termsKind[kind];
+    d--;
+
+    if (m > 12) {
+      if (m % 12 === 0) {
+        y += parseInt(m / 12) - 1;
+        m = 12;
+      } else {
+        y += parseInt(m / 12);
+        m = m % 12;
+      }
+    }
+    if (d === 0) {
+      m--;
+      d = 28;
+      if (m === 0) {
+        y--;
+        m = 12;
+      }
+    }
+    if (y < current[0]) {
+      result.push(i + 1);
+      return;
+    } else if (y === current[0]) {
+      if (m < current[1]) {
+        result.push(i + 1);
+        return;
+      } else if (m === current[1]) {
+        if (d < current[2]) {
+          result.push(i + 1);
+          return;
+        }
+      }
+    }
+  });
+  return result;
+}
+
+// 신고 결과 받기
+function solution(id_list, report, k) {
+  var answer = [];
+  return answer;
+}
