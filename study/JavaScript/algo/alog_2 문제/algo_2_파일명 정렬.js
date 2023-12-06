7dbe6bca0aa4e5276ea9ad6c3c07b2cc6b504c3e
lugia574 <66871221+lugia574@users.noreply.github.com>
2023-12-06 17:01:58 +0900

    algo - 케익자르기

--- study/JavaScript/algo/alog_2 문제/algo_2_파일명 정렬.js
+++ study/JavaScript/algo/alog_2 문제/algo_2_파일명 정렬.js
@@ -1,5 +1,13 @@
-// 이름만 봐도 개같을꺼 같은데
+// 이름만 봐도 개같을꺼 같은데 정렬이래 ㅅㅂ ㅋ
+// 이걸 어떻게 짬??
+// 이게 데이터가 딱딱 부위가 정해져 있으면 모르는데 아니자너
+// 예제를 보면 대충 비슷비슷하긴해 아예 다른 놈들끼리 정렬하라는 소린 아닌거 같애
 function solution(files) {
-  var answer = [];
+  let answer;
+  let head;
+  let number;
+  let tail;
+
+  files[0].map((e) => {});
   return answer;
 }
