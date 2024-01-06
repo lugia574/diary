228822a75e6d91c3a1f855a6f4f4ccec335bac45
lugia574 <66871221+lugia574@users.noreply.github.com>
2024-01-06 18:02:19 +0900

    algo

--- study/JavaScript/algo/alog_2 문제/algo_2_연속된 부분 수열의 합.js
+++ study/JavaScript/algo/alog_2 문제/algo_2_연속된 부분 수열의 합.js
@@ -15,6 +15,30 @@ function solution(sequence, k) {
   return answer[0];
 }
 
+// 투 포인트로 다시 풀어야해
+function solution(sequence, k) {
+  let answer = [];
+  const len = sequence.length;
+  const prePix = new Array(len + 1).fill(0);
+  prePix[1] = sequence[0];
+  for (let i = 1; i < len; i++) prePix[i + 1] = sequence[i] + prePix[i];
+
+  let left = 0;
+  let right = 1;
+
+  while (left <= right) {
+    const seqSum = prePix[right] - prePix[left];
+    if (seqSum === k) {
+      answer.push([left, right - 1]);
+      left++;
+    } else if (seqSum < k) right++;
+    else left++;
+  }
+
+  answer.sort((a, b) => a[1] - a[0] - (b[1] - b[0]));
+  return answer[0];
+}
+
 const sequence = [1, 2, 3, 4, 5];
 const k = 7;
 const result = [2, 3];
